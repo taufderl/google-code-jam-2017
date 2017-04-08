b00ed4baabd26ee7b93edf1d0a83a3eccc613b32
tadl <tadl@taufderl.de>
2017-04-09 01:31:51 +0200

    Improved C performance, but not enough

--- C.py
+++ C.py
@@ -2,51 +2,56 @@
 
 import sys
 
-def get_LR(B):
-  L = [None] * len(B)
-  for i in range(len(B)):
-    if B[i]:
-      L[i] = -1 # occupied
-    else:
-      L[i] = L[i-1]+1
-  R = [None]*len(B)
-  for i in reversed(range(len(B))):
-    if B[i]:
-      R[i] = -1
-    else:
-      R[i] = R[i+1]+1
-  return L, R
+def eprint(x):
+  print(x, file=sys.stderr)
 
 
-def new_user(B):
-  L, R = get_LR(B)
-  MIN = [min(L[i], R[i]) for i in range(len(B))]
-  MAX = [max(L[i], R[i]) for i in range(len(B))]
-  COMBO = [(MIN[i], MAX[i]) for i in range(len(B))]
-  c = max(COMBO)
-  y, x = c
-  index = COMBO.index(c)
-  """
-  print('B  ', B)
-  print('L  ', L)
-  print('R  ', R)
-  print('MIN', MIN)
-  print('MAX', MAX)
-  print('COMBO', COMBO)
-  print(max(COMBO), COMBO.index(max(COMBO)))
-  print(10*'-')
-  """
-  return index, x, y
-
-
-def calculate(data):
+def calculate2(data):
   data = data.split(' ')
   N, K = int(data[0]), int(data[1])
-  B = [True] + N*[False] + [True]
+  if N == K:
+    return "0 0"
+  # initialize LR and priolist
+  L = [-1] + list(range(0, N)) + [-1]
+  R = [-1] + list(range(N-1, 0-1, -1)) + [-1]
+  MINMAX = [(min(L[i],R[i]), max(L[i],R[i])) for i in range(len(L))]
+  assert(len(L) == len(R))
   for i in range(K):
-    index, x, y = new_user(B)
-    #print(index, x, y)
-    B[index] = True
+    #eprint("*********ITERATION " + str(i) + " ITERATION*********")
+    #eprint('L  ', L)
+    #eprint('R  ', R)
+    #eprint('MINMAX', MINMAX)
+    #eprint(10*'-')
+    c = max(MINMAX)
+    #eprint(c)
+    y, x = c
+    new_user = MINMAX.index(c)
+    #eprint(new_user)
+    eprint("%i %s"%(new_user, str(c)))
+    L[new_user] = -1
+    R[new_user] = -1
+    MINMAX[new_user] = (-1, -1)
+    # update all in L from new_user til next found user
+    eprint("                     START: "+str(new_user+1))
+    for i in range(new_user+1, len(L)):
+      if L[i] == -1: # found next user, break
+        eprint("                       END: "+str(i))
+        break
+      else:
+        L[i] = L[i-1]+1
+        #eprint("OLD     %s"%str(MINMAX[i]))
+        MINMAX[i] = (min(L[i],R[i]), max(L[i],R[i]))
+        #eprint("UPDATED %s"%str(MINMAX[i]))
+    # update all in R from new_user towards the left
+    for i in range(new_user-1, 0-1, -1):
+      if R[i] == -1:
+        break
+      else:
+        R[i] = R[i+1]+1
+        MINMAX[i] = (min(L[i],R[i]), max(L[i],R[i]))
+    #eprint('MINMAX', MINMAX)
+
+
   return "{} {}".format(x, y)
   
 
@@ -57,17 +62,19 @@ def process_file(filename):
   count, data = int(data[0]), [x.strip() for x in data]
   i = 1
   while i <= count:
-    print("Case #{}: {}".format(i, calculate(data[i])))
-    print("Case #{}: {}".format(i, calculate(data[i])), file=sys.stderr)
+    #eprint(data[i])
+    result = calculate2(data[i])
+    print("Case #{}: {}".format(i, result))
+    eprint("Case #{}: {}".format(i, result))
     i += 1
 
 
 def main():
   filename = 'C-my.in'
-  filename = 'C-small-1-attempt0.in'
+  #filename = 'C-small-1-attempt0.in'
   #filename = 'C-small-1-attempt1.in'
   #filename = 'C-small-1-attempt2.in'
-  #filename = 'C-small-2-attempt0.in'
+  filename = 'C-small-2-attempt0.in'
   #filename = 'C-large.in'
   process_file(filename)
 
